23da6113f3d32985acab1a47118aa6d646b9c4c0
BilakshanP <pbilakshan@gmail.com>
2022-10-01 19:48:16 +0530

    cool

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,26 +1,19 @@
 #include "print.h"
 
-// int fib[] = {0, 1};
-// 
-//     for(int i{0}; i < 30; i++){
-//         fib[2] = fib[0] + fib[1];
-//         print(fib[2]);
-//         fib[0] = fib[1];
-//         fib[1] = fib[2];
-//     }
-
-
 int *fibonacci(int number){
-    static int fib[32] = {1, 1, 0};
-    for(int i{0}; i < 32; i++){
+    static int fib[64] = {1, 1, 0};
+    for(int i{0}; i < number-2; i++){
         fib[2+i] = fib[1+i] + fib[i];
     };
     return fib;
 }
 
 int main(){
-    int *fib = fibonacci(0);
-    for(int i{0}; i < 32; i++){
-        print(i, ""); print(*(fib+i));
+    int upto{32};
+
+    int *fib = fibonacci(upto);
+    for(int i{0}; i < upto; i++){
+        print(i+1, " ");
+        print(*(fib+i));
     }
 }
--- permutations.cpp
+++ permutations.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 
-// void a_multiply(auto value, int times){
-//     for(int i{1}; i <= times; i++){
-//         std::cout << value; 
-//     }
-// }
-
 int main(){
-    const char *str = "Hello";
-    const int c = 1;
-    const int *pconst = &c;
-    const int c2 = 2;
-    const int *const pconst2 = &c;
-    std::cout << *pconst2 <<" "<< pconst2 << std::endl
-              << *pconst  <<" "<< pconst;
+    //
 }
